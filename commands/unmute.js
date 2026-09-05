a46d69d172b9addca7870f794dcb4b02d611b729
Hector Manuel <nathanielkango@gmail.com>
2026-09-05 03:55:49 +0000

    Mirror replacement: delete commands/unmute.js

--- commands/unmute.js
+++
@@ -1,7 +0,0 @@
-const { getLang } = require('../lib/lang');
-async function unmuteCommand(sock, chatId) {
-    await sock.groupSettingUpdate(chatId, 'not_announcement'); // Unmute the group
-    await sock.sendMessage(chatId, { text: getLang(sock).mute_unmuted });
-}
-
-module.exports = unmuteCommand;
